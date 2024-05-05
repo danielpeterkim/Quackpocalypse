1b08ec4fbcbd2f447d7c7fecf8989b3b8008986d
sanya mann <sanyamann03@gmail.com>
2024-05-05 17:11:49 -0400

    routes and shi (in progress)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Map from './Map';
+import Home from './Home';
+import Lobby from './Lobby';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <div className="homepage">
+      {/* <div className="homepage">
         <h1>Quackpocalypse</h1>
         <div className="options">
           <button>Create a Room</button>
           <Link to="/map"><button>Join a Room</button></Link>
         </div>
         <button className="info-button">Info</button>
-      </div>
+      </div> */}
       <Routes>
-        <Route path="/map" element={<Map />} />
+        <Route path='/' element={<Home />} /> 
+        <Route path='/lobby/' element = {<Lobby />} /> 
+        <Route path='/map' element={<Map />} />
       </Routes>
     </div>
   );
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,30 @@
-import express from "express";;
+import express from 'express';
+import { createRoom, joinRoom, findRoom } from "../db/roomModule.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.status(200).send("This is a server!");
+});
+
+router.post('/create-room', async (req, res) => {
+    try {
+        const { name, hiddenHands, epidemicCards } = req.body;
+        const roomId = await createRoom(name, hiddenHands, epidemicCards);
+        res.json({ roomId });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.post('/join-room', async (req, res) => {
+    try {
+        const { name, roomCode } = req.body;
+        const roomId = await joinRoom(name, roomCode);
+        res.json({ roomId });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 });
 
 const setupRoutes = (app) => {
