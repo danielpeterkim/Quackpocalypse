3018726bd219fb363123ffae1e2b99029fbdf3e9
danielpeterkim <125161100+danielpeterkim@users.noreply.github.com>
2024-05-08 23:21:20 -0400

    sockets

--- server/server.js
+++ server/server.js
@@ -22,41 +22,41 @@ const server = app.listen(3000, async () => {
     console.log("We now have a server at http://localhost:3000/ !");
 });
 
-// const io = new Server(server, {
-//     cors: {
-//         origin: "*", // Adjust this to match your client URL
-//         methods: ["GET", "POST"],
-//     },
-// });
+const io = new Server(server, {
+    cors: {
+        origin: "*", // Adjust this to match your client URL
+        methods: ["GET", "POST"],
+    },
+});
 
-// io.on("connection", (socket) => {
-//     console.log("New client connected");
+io.on("connection", (socket) => {
+    console.log("New client connected");
 
-//     socket.on("join-room", (roomId) => {
-//         socket.join(roomId);
-//         console.log(`Client joined room: ${roomId}`);
-//     });
+    socket.on("join-room", (roomId) => {
+        socket.join(roomId);
+        console.log(`Client joined room: ${roomId}`);
+    });
 
-//     socket.on("offer", (roomId, offer) => {
-//         socket.to(roomId).emit("offer", offer);
-//     });
+    socket.on("offer", (roomId, offer) => {
+        socket.to(roomId).emit("offer", offer);
+    });
 
-//     socket.on("answer", (roomId, answer) => {
-//         socket.to(roomId).emit("answer", answer);
-//     });
+    socket.on("answer", (roomId, answer) => {
+        socket.to(roomId).emit("answer", answer);
+    });
 
-//     socket.on("ice-candidate", (roomId, candidate) => {
-//         socket.to(roomId).emit("ice-candidate", candidate);
-//     });
+    socket.on("ice-candidate", (roomId, candidate) => {
+        socket.to(roomId).emit("ice-candidate", candidate);
+    });
 
-//     socket.on("send-message", (roomId, message) => {
-//         console.log("Received message:", message);
-//         socket.to(roomId).emit("message", message);
-//     });
+    socket.on("send-message", (roomId, message) => {
+        console.log("Received message:", message);
+        socket.to(roomId).emit("message", message);
+    });
 
-//     socket.on("disconnect", () => {
-//         console.log("Client disconnected");
-//     });
-// }
-// );
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+}
+);
 console.log("Hello world")
